0527f7ec06eb73f923df51d92f6bc935ef12ce79
0xUntainted <109836089+UntaintedTech@users.noreply.github.com>
2023-08-01 22:23:34 +0800

    Update EmailSender.py

--- EmailSender.py
+++ EmailSender.py
@@ -4,14 +4,15 @@ from email.message import EmailMessage
 email = EmailMessage()
 email['from'] = 'insert_your_name_here'
 email['to'] = 'enter_name_@gmail.com'
-email['subject'] = 'enter_subject_here!'
+email['subject'] = 'enter_email_subject_here'
 
 email.set_content('type your Message here')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('enter_your_email_@gmail.com', 'google_generated_password')# gmail apps_password is used
+    # google account-generated apps password is used here
+    smtp.login('enter_your_email_@gmail.com', 'google_generated_password')
     smtp.send_message(email)
     print('Email sent!')
 
